b89396a69cde14a7c4ff5196f8fe580242ecd267
Alexis Williams <alexis@typedr.at>
2023-02-16 11:11:07 -0800

    Unify formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  output: "server",
-  adapter: node({
-    mode: 'standalone'
-  }),
+    integrations: [react()],
+    output: "server",
+    adapter: node({
+        mode: "standalone",
+    }),
 });
