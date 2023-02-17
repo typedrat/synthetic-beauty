795828cfcf28187c633235478694a222851790b5
Alexis Williams <alexis@typedr.at>
2023-02-16 16:13:11 -0800

    Set up prettier correctly

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    plugins: [require.resolve("prettier-plugin-astro")],
+    overrides: [
+        {
+            files: "*.astro",
+            options: {
+                parser: "astro",
+            },
+        },
+    ],
+    tabWidth: 4,
+    bracketSameLine: false,
+};
