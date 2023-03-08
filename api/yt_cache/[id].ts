cc65db4f66bc32c7a6fa84bb297d81e9a15db050
Alexis Williams <alexis@typedr.at>
2023-03-08 14:44:02 -0800

    Test using serverless functions explicitly.

--- api/yt_cache/[id].ts
+++ api/yt_cache/[id].ts
@@ -1,4 +1,4 @@
-import type { APIContext } from "astro";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 import {
     S3Client,
     HeadObjectCommand,
@@ -9,18 +9,24 @@ import { Upload } from "@aws-sdk/lib-storage";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { Innertube } from "youtubei.js";
 
-const Bucket = import.meta.env.AUDIO_CACHE_BUCKET_NAME;
+const Bucket = process.env.AUDIO_CACHE_BUCKET_NAME;
 const expiresIn = 7 * 24 * 60 * 60;
 
 const client = new S3Client({});
 const youtube = await Innertube.create();
 
-export async function get(ctx: APIContext) {
+export default async function handler(
+    request: VercelRequest,
+    response: VercelResponse
+) {
     if (!Bucket) {
         throw new Error("AUDIO_CACHE_BUCKET_NAME should not be undefined!");
     }
 
-    const id = ctx.params.id!;
+    const id = request.query.id;
+    if (typeof id !== "string") {
+        throw new Error(`Invalid source ID: ${id}`);
+    }
 
     const Key = `${id}.mp4`;
 
@@ -48,11 +54,5 @@ export async function get(ctx: APIContext) {
     const getCommand = new GetObjectCommand({ Bucket, Key });
     const url = await getSignedUrl(client, getCommand, { expiresIn });
 
-    return new Response(JSON.stringify({ url, expiresIn }), {
-        status: 200,
-        statusText: "OK",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
+    response.status(200).json({ url, expiresIn });
 }
