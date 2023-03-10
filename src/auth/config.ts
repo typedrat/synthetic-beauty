772049196d927a38716e7431dc2b3941b6377089
Alexis Williams <alexis@typedr.at>
2023-03-09 18:50:39 -0800

    Configure email provider

--- src/auth/config.ts
+++ src/auth/config.ts
@@ -1,6 +1,30 @@
+import Email from "@auth/core/providers/email";
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBAdapter } from "@next-auth/dynamodb-adapter";
+
 import type { AstroAuthConfig } from "./server";
 
+const dynamodbClient = DynamoDBDocument.from(new DynamoDB({}), {
+    marshallOptions: {
+        convertEmptyValues: true,
+        removeUndefinedValues: true,
+        convertClassInstanceToMap: true,
+    },
+});
+
 export const authConfig: AstroAuthConfig = {
-    providers: [],
+    providers: [
+        // Email oesn't work, not sure why.
+        Email({
+            server: import.meta.env.EMAIL_SERVER,
+            from:
+                import.meta.env.EMAIL_FROM ??
+                "Synthetic Beauty <noreply@synthetic.beauty>",
+        }),
+    ],
+    adapter: DynamoDBAdapter(dynamodbClient, {
+        tableName: import.meta.env.AUTH_TABLE_NAME,
+    }),
     debug: import.meta.env.DEV,
 };
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,7 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-    SPOTIFY_CLIENT_ID?: string;
-    SPOTIFY_CLIENT_SECRET?: string;
-    ENCRYPTION_SECRET?: string;
-    ENCRYPTION_ALG?: string;
     AUDIO_CACHE_BUCKET_NAME?: string;
+    EMAIL_SERVER?: string;
+    EMAIL_FROM?: string;
 }
